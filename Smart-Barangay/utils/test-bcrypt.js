376704c92d5bb29923cc9f30dcf81a3437c307cc
dreeiii <191374786+dreeiii@users.noreply.github.com>
2026-04-30 17:43:52 +0800

    Delete Smart-Barangay directory DUPLICATED

--- Smart-Barangay/utils/test-bcrypt.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs');
-const hash = '$2a$06$hAWbe6BuBaRG50aVGp8G2esji'; // Use the full hash from your DB
-const password = '12345';
-
-bcrypt.compare(password, hash, (err, res) => {
-  if (err) throw err;
-  console.log('Password matches:', res);
-});
